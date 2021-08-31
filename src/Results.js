efbccd8104d751591280fc5bfe8db5854d2126c7
AnnaKnol <83373222+AnnaKnol@users.noreply.github.com>
2021-08-31 17:47:41 +0200

    fix empty phonetic object bug

--- src/Results.js
+++ src/Results.js
@@ -6,11 +6,12 @@ import Origin from "./Origin";
 import "./Results.css";
 
 export default function Results(props) {
+  console.log(props.results);
   return (
     <div className="Results">
       <h2>{props.results.word}</h2>
       {props.results.phonetics.map(function (phonetic, index) {
-        if (props.results.phonetics[0]) {
+        if (Object.keys(props.results.phonetics[0]).length > 0) {
           return (
             <div key={index}>
               <Phonetic phonetic={phonetic} />
