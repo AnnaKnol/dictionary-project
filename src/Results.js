b66e11b9a5e4fd7a50fbe9db7d40b2c1f9920853
AnnaKnol <83373222+AnnaKnol@users.noreply.github.com>
2021-09-01 18:35:47 +0200

    delete console.log

--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,6 @@ import Origin from "./Origin";
 import "./Results.css";
 
 export default function Results(props) {
-  console.log(props.results);
   return (
     <div className="Results">
       <h2>{props.results.word}</h2>
