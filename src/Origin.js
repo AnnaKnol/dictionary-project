eb6caca239298d1eac1584eb7c27579dab7a0671
AnnaKnol <83373222+AnnaKnol@users.noreply.github.com>
2021-10-02 15:13:40 +0200

    add option for showing 'origin'

--- src/Origin.js
+++ src/Origin.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Origin.css";
 
 export default function Origin(props) {
+  const [originShowing, setOriginShowing] = useState(false);
+
+  function handleClick(event) {
+    event.preventDefault();
+    setOriginShowing(!originShowing);
+  }
+
   if (props.origin) {
-    return (
-      <div className="Origin">
-        <strong>Origin:</strong> {props.origin}
-      </div>
-    );
+    if (originShowing) {
+      return (
+        <div className="Origin">
+          <a href="/" onClick={handleClick}>
+            Origin:
+          </a>{" "}
+          {props.origin}
+        </div>
+      );
+    } else {
+      return (
+        <div className="Origin">
+          <a href="/" onClick={handleClick}>
+            Origin
+          </a>
+        </div>
+      );
+    }
   } else {
     return null;
   }
